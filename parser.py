69fff89b45fc1b70640cc269a34c6a7688c63873
Miraneh <najafi.mehraneh@gmail.com>
2020-11-27 13:56:35 +0330

    token edit

--- parser.py
+++ parser.py
@@ -12,6 +12,5 @@ parser = Lark.open(grammar_file, rel_to=__file__, parser="lalr")
 
 if __name__ == "__main__":
     # text = input()
-    text = """
-    """
+    text = """ "boba" """
     print(parser.parse(text).pretty())
