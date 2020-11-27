9267390ca9a860e32ec8ad5a28c87681d6cd86dd
savaw <savaw97@gmail.com>
2020-11-27 12:55:05 +0330

    Edit tokens

--- parser.py
+++ parser.py
@@ -6,20 +6,13 @@ logger.setLevel(logging.DEBUG)
 from pathlib import Path
 
 grammer_path = Path(__file__).parent
-grammar_file = grammer_path / 'grammer.lark'
+grammar_file = grammer_path / 'tokens.lark'
 
 parser = Lark.open(grammar_file, rel_to=__file__, parser="lalr")
 
 if __name__ == "__main__":
     # text = input()
     text = """
-string T_ID() {
-   return 1;
-}
-
-T_ID T_ID() {
-   T_ID T_ID;
-   return T_ID;
- }
+    true
     """
     print(parser.parse(text).pretty())
