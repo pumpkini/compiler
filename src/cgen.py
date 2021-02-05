d33ccdc6f0f3168bce44f197f1dfd9eb00243fe6
rostaroghani <rrosta@yahoo.com>
2021-02-05 23:20:05 +0330

    var decl + cgen back

--- src/cgen.py
+++ src/cgen.py
@@ -11,12 +11,38 @@ grammer_file = grammer_path / 'grammer.lark'
 
 parser = Lark.open(grammer_file, rel_to=__file__, parser="lalr")
 
+class Cgen(Interpreter):
+    def visit(self, tree, *args, **kwargs):
+        f = getattr(self, tree.data)
+        wrapper = getattr(f, 'visit_wrapper', None)
+        if wrapper is not None:
+            return f.visit_wrapper(f, tree.data, tree.children, tree.meta)
+        else:
+            return f(tree, *args, **kwargs)
+
+    def visit_children(self, tree, *args, **kwargs):
+        return [self.visit(child, *args, **kwargs) if isinstance(child, Tree) else child
+                for child in tree.children]
+
+    def __default__(self, tree, *args, **kwargs):
+        return self.visit_children(tree, *args, **kwargs)
+
+    ### Cgen methods
+    def program(self, tree, *args, **kwargs):
+        print("hehehe")
+
+    def decl(self, tree, *args, **kwargs):
+        code = ''
+        for decl in tree.children:
+            if decl.data == 'variable_decl':
+                code += self.visit(decl)
+        return code
 
 def generate_tac(code):
 	try:
 		tree = parser.parse(code)
-		root_symbol_table = SymbolTable()
-		Cgen().visit(tree,  symbol_table= root_symbol_table)
+		# root_symbol_table = SymbolTable()
+		# Cgen().visit(tree,  symbol_table= root_symbol_table)
 		
 	except ParseError as e:
 		# TODO
