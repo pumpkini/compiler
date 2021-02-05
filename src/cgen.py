1e81571867005b43c4848bda32bdc9ecc99eeb3e
rostaroghani <rrosta@yahoo.com>
2021-02-06 02:11:41 +0330

    while cgen

--- src/cgen.py
+++ src/cgen.py
@@ -12,47 +12,56 @@ grammer_file = grammer_path / 'grammer.lark'
 parser = Lark.open(grammer_file, rel_to=__file__, parser="lalr")
 
 class Cgen(Interpreter):
-    def visit(self, tree, *args, **kwargs):
-        f = getattr(self, tree.data)
-        wrapper = getattr(f, 'visit_wrapper', None)
-        if wrapper is not None:
-            return f.visit_wrapper(f, tree.data, tree.children, tree.meta)
-        else:
-            return f(tree, *args, **kwargs)
-
-    def visit_children(self, tree, *args, **kwargs):
-        return [self.visit(child, *args, **kwargs) if isinstance(child, Tree) else child
+	def visit(self, tree, *args, **kwargs):
+		f = getattr(self, tree.data)
+		wrapper = getattr(f, 'visit_wrapper', None)
+		if wrapper is not None:
+			return f.visit_wrapper(f, tree.data, tree.children, tree.meta)
+		else:
+			return f(tree, *args, **kwargs)
+	
+	def visit_children(self, tree, *args, **kwargs):
+		return [self.visit(child, *args, **kwargs) if isinstance(child, Tree) else child
                 for child in tree.children]
-
-    def __default__(self, tree, *args, **kwargs):
-        return self.visit_children(tree, *args, **kwargs)
+	
+	def __default__(self, tree, *args, **kwargs):
+		return self.visit_children(tree, *args, **kwargs)
 
     ### Cgen methods
-    def program(self, tree, *args, **kwargs):
-        print("hehehe")
-
-    def decl(self, tree, *args, **kwargs):
-        code = ''
-        for decl in tree.children:
-            if decl.data == 'variable_decl':
-                code += self.visit(decl)
-        return code
-
-def generate_tac(code):
-	try:
-		tree = parser.parse(code)
-		# root_symbol_table = SymbolTable()
-		# Cgen().visit(tree,  symbol_table= root_symbol_table)
+	def program(self, tree, *args, **kwargs):
+		print("hehehe")
 		
-	except ParseError as e:
-		# TODO
-		pass
+	def while_stmt(self, tree):
+		code = '\t.text\n'
+		code += "loop:\t"
+		condition_code = self.visit(tree.children[0])
+		stmt_code = self.visit(tree.children[1])
+		code += condition_code
+		code += "\n\tj\tend"
+		code += "\nbody:\t"
+		code += stmt_code
+		code += "\n\tj\tloop"
+		code += "\n\tend:"
+		# code += TODO end 
+		return code
+
+	    
+
+# def generate_tac(code):
+# 	try:
+# 		tree = parser.parse(code)
+# 		# root_symbol_table = SymbolTable()
+# 		# Cgen().visit(tree,  symbol_table= root_symbol_table)
+		
+# 	except ParseError as e:
+# 		# TODO
+# 		pass
 	
 
-if __name__ == "__main__":
-	inputfile = '../tmp/in1.d'
-	code = ""
-	with open(inputfile, "r") as input_file:
-		code = input_file.read()
+# if __name__ == "__main__":
+# 	inputfile = '../tmp/in1.d'
+# 	code = ""
+# 	with open(inputfile, "r") as input_file:
+# 		code = input_file.read()
 
-	generate_tac(code)
+# 	generate_tac(code)
