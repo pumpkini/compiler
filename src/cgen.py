614808712289802ead1489b9b66d3bfcf0165164
rostaroghani <rrosta@yahoo.com>
2021-02-05 22:50:13 +0330

    cgen back

--- src/cgen.py
+++ src/cgen.py
@@ -11,16 +11,51 @@ grammer_file = grammer_path / 'grammer.lark'
 
 parser = Lark.open(grammer_file, rel_to=__file__, parser="lalr")
 
+class Cgen(Interpreter):
+    def visit(self, tree, *args, **kwargs):
+        f = getattr(self, tree.data)
+        wrapper = getattr(f, 'visit_wrapper', None)
+        if wrapper is not None:
+            return f.visit_wrapper(f, tree.data, tree.children, tree.meta)
+        else:
+            return f(tree, *args, **kwargs)
 
-def generate_tac(code):
-	try:
-		tree = parser.parse(code)
-		root_symbol_table = SymbolTable()
-		Cgen().visit(tree,  symbol_table= root_symbol_table)
+    def visit_children(self, tree, *args, **kwargs):
+        return [self.visit(child, *args, **kwargs) if isinstance(child, Tree) else child
+                for child in tree.children]
+
+    def __default__(self, tree, *args, **kwargs):
+        return self.visit_children(tree, *args, **kwargs)
+
+    ### Cgen methods
+    def program(self, tree, *args, **kwargs):
+        print("hehehe")
+
+	def decl(self,tree):
+		code = ""
+		for decl in tree.children:
+			if decl.data == "variable_decl":
+				code += self.visit(decl)
+			elif decl.data == "function_decl":
+				pass
+				#TODO
+			elif decl.data == "class_decl":
+				pass
+				#TODO
+			else:
+				pass
+				#TODO
+
+
+# def generate_tac(code):
+# 	try:
+# 		tree = parser.parse(code)
+# 		root_symbol_table = SymbolTable()
+# 		Cgen().visit(tree,  symbol_table= root_symbol_table)
 		
-	except ParseError as e:
-		# TODO
-		pass
+# 	except ParseError as e:
+# 		# TODO
+# 		pass
 	
 
 if __name__ == "__main__":
@@ -29,4 +64,4 @@ if __name__ == "__main__":
 	with open(inputfile, "r") as input_file:
 		code = input_file.read()
 
-	generate_tac(code)
+	# generate_tac(code)
