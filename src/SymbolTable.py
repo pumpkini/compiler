649083e1fac6fd3d2ee92d0e4d2d82a450d7575d
rostaroghani <rrosta@yahoo.com>
2021-02-05 23:11:09 +0330

    Revert "cgen back"
    This reverts commit 3c7780c66cc5eda6e1685ba0891b8eeab314111f.

--- src/SymbolTable.py
+++ src/SymbolTable.py
@@ -19,3 +19,36 @@ class SymbolTable():
         self.variables = {}
         self.parent = None
         SymbolTable.symbol_tables.append(self)
+
+
+class Cgen(Interpreter):
+    def visit(self, tree, *args, **kwargs):
+        f = getattr(self, tree.data)
+        wrapper = getattr(f, 'visit_wrapper', None)
+        if wrapper is not None:
+            return f.visit_wrapper(f, tree.data, tree.children, tree.meta)
+        else:
+            return f(tree, *args, **kwargs)
+
+    def visit_children(self, tree, *args, **kwargs):
+        return [self.visit(child, *args, **kwargs) if isinstance(child, Tree) else child
+                for child in tree.children]
+
+    def __default__(self, tree, *args, **kwargs):
+        return self.visit_children(tree, *args, **kwargs)
+
+    ### Cgen methods
+    def program(self, tree, *args, **kwargs):
+        symbol_table = kwargs.get('symbol_table')
+        print(symbol_table)
+        print(symbol_table.variables)
+        print("hehehe")
+
+    def decl(self, tree, *args, **kwargs):
+        code = ''
+        symbol_table = kwargs.get('symbol_table')
+        for decl in tree.children:
+            if decl.data == 'variable_decl' or decl.data == 'function_decl' or decl.data == 'class_decl':
+                code += self.visit(decl)
+        return code
+
