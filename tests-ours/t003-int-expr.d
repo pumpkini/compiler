58aaba0634d29595d0a91201c62f22598d48e25b
savaw <savaw97@gmail.com>
2021-02-12 00:24:18 +0330

    Add test

---
+++ tests-ours/t003-int-expr.d
@@ -0,0 +1,14 @@
+int main() {
+    int a;
+    int b;
+    a = 1;
+    b = 3;
+    Print(a + b >= 1);
+    Print(a + b == 4);
+    Print(a + b == 5);
+    Print(5 != a + b);
+    Print(4 != a + b);
+    Print(a + 2 <= b);
+    Print(a + 3 <= b);
+    Print(a + 5 * 2 == b * 10 - 19);
+}
