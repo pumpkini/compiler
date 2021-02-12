3ae8723b6c42880185730c062beb275c376232ce
savaw <savaw97@gmail.com>
2021-02-12 12:27:39 +0330

    Add test for user input

---
+++ tests-ours/t008-userinput.d
@@ -0,0 +1,18 @@
+int main() {
+    int a;
+    int b;
+    int c;
+   string d;
+    string e;
+    string f;
+   a = ReadInteger();
+   b = ReadInteger();
+   c = a * b;
+   
+   Print(a, " ", b, " ", c);
+    
+    d = ReadLine();
+    e = ReadLine();
+    f = d + e;
+    Print(d, e, f);
+}
