d9cb413ee40f907d62ff52dece65f4421f8946c2
rostaroghani <rrosta@yahoo.com>
2021-02-13 20:52:56 +0330

    test added for expr

---
+++ tests-ours/t011-intdoubleexpr.d
@@ -0,0 +1,19 @@
+int main() {
+    int a;
+    int b;
+    double c;
+    double d;
+    a = 9;
+    b = 4;
+    c = 4.4;
+    d = 2.;
+    Print("sum",a + b);
+    Print("mul",a * b);
+    Print("sub",a - b);
+    Print("div",a / b);
+    Print("mod",a % b);
+    Print("sum",c + d);
+    Print("mul",c * d);
+    Print("sub",c - d);
+    Print("div",c / d);
+}
