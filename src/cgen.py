946bc3fde9e2765edaef43e3a98002a4e8232bfb
rostaroghani <rrosta@yahoo.com>
2021-02-16 02:02:04 +0330

    arr add

--- src/cgen.py
+++ src/cgen.py
@@ -1191,7 +1191,7 @@ class Cgen(Interpreter):
 
 				""".replace("\t\t\t","")
 
-		elif var1.type_.name == "array" and var1.type_.arr_type.name == var2.type_.arr_type.name:
+		elif var1.type_.name == "array" and var1.type_.arr_type.are_equal(var2.type_.arr_type):
 			lab_num = IncLabels()
 			code += f"""
 				### add array[int]
